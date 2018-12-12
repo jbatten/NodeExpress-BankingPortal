cd2865c1569b821104083cf247f5ab7e664cdcdf
James Batten <james.batten@gmx.com>
2018-12-12 10:45:35 +0000

    Completed the module

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.get('/credit', (req, res) => {
 });
 
 app.get('/transfer', (req, res) => res.render('transfer'));
-app.post('transfer', (req, res) => {
+app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
     const accountsJSON = JSON.stringify(accounts, null, 4);
